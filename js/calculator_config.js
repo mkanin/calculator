6ad793816a875e08964fd749142762a4a7fe465a
Mikhail Kanin <contact@mkanin.com>
2017-05-27 12:21:04 +0000

    Add existing file

---
+++ js/calculator_config.js
@@ -0,0 +1,27 @@
+/*
+*  Calculator of painting of building constructions' costs (github.com/mkanin/calculator)
+*
+*  See LICENSE.txt for licensing information.
+*
+*  Copyright (c) 2017 Mikhail Kanin
+*/
+
+// Price
+var price = 10.0;
+var currency = "$";
+
+// Default digital values
+var defaultInputValue = "0.0";
+var defaultResultValue = "0.00";
+
+// Styles
+var staticAdditionalDivColorClassesNames = ["static-additional-class"];
+var staticAdditionalDivResizeClassesNames = ["product-resize"];
+var staticAdditionalImgResizeClassesNames = ["image-resize"];
+var titleClassesNames = ["input-title"];
+var inputClassesNames = ["input-field-main"];
+var resultTitleClassName = "output-result-title";
+var resultMainClassesNames = ["output-result-main"];
+
+// Directory of images
+var imgDir = "images/";
